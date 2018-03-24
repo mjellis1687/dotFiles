4ff0b55ded38a6fa855a3d3a18b9770845105a99
Matt Ellis <ellis.matt.j@gmail.com>
2018-03-24 16:05:24 -0500

    Added in laptop hostname in get git properties script

--- bin/findGitConfig.sh
+++ bin/findGitConfig.sh
@@ -6,7 +6,7 @@
 
 editor()
 {
-	if [ "${HOSTNAME}" = "matty-2016" ] || [ "${HOSTNAME}" = "matty" ]; then
+	if [ "${HOSTNAME}" = "matty-2016" ] || [ "${HOSTNAME}" = "matty-Gazelle-Professional" ]; then
 		ED="vim"
 	else
 		ED="/cygdrive/c/cygdrive/bin/vim.exe"
@@ -17,7 +17,7 @@ credential()
 {
 	if [ "${HOSTNAME}" = "matty-2016" ]; then
 		CR="/usr/share/git/crendential/gnome-keyring/git-credential-gnome-keyring"
-	elif [ "${HOSTNAME}" = "matty" ]; then
+	elif [ "${HOSTNAME}" = "matty-Gazelle-Professional" ]; then
 		CR="/usr/share/doc/git/contrib/credential/gnome-keyring/git-credential-gnome-keyring"
 	else
 		CR="/cygdrive/C/Users/cellism7/AppData/Local/GitHub/PortableGit_f02737a78695063deace08e96d5042710d3e32db/mingw32/libexec/git-core/git-credential-manager.exe"
